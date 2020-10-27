689d8a19a950a583e3b7e73c4df21e010e5a16ea
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-10-27 18:24:05 +0100

    Validation for empty and less than 2 characters.

---
+++ script.js
@@ -0,0 +1,71 @@
+const validation = {
+  minLength: {
+    length: 2,
+    errorMessage: `At least ${this.length} characters`,
+    validate: (text) => text.length >= 2,
+  },
+  notEmpty: {
+    errorMessage: `A value is required`,
+    validate: (text) => text && text !== "",
+  },
+};
+
+function renderErrorMessages(target, errorMessages) {
+  for (message of errorMessages) {
+    target.insertAdjacentHTML("afterend", `<div class="errorMessage">${message}</div>`);
+  }
+}
+
+function validatorHandler(target) {
+  // If no validation rules for this target element.
+  if (!target.dataset.validators) {
+    return;
+  }
+
+  // Clear earlier messages
+  target.parentElement.querySelectorAll(".errorMessage").forEach((e) => e.remove());
+  target.classList.remove("error");
+
+  const errorMessages = validate(target);
+  if (errorMessages.length > 0) {
+    target.classList.add("error");
+    renderErrorMessages(target, errorMessages);
+  }
+  return errorMessages;
+}
+
+function validate(target) {
+  const validators = target.dataset.validators.split(" ");
+  const errorMessages = [];
+  for (let validator of validators) {
+    let isValid = true;
+    isValid = validation[validator].validate(target.value);
+    if (!isValid) {
+      errorMessages.push(validation[validator].errorMessage);
+    }
+  }
+  return errorMessages;
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  let errorMessages = [];
+  for (let i = 0; i < e.currentTarget.length; i++) {
+    const messages = validatorHandler(e.target[i]);
+    if (messages) errorMessages = errorMessages.concat(messages);
+  }
+  if (errorMessages.length > 0) {
+    console.log("Do not send");
+  } else {
+    console.log("Sent!");
+  }
+}
+
+function main() {
+  document.querySelector("form").addEventListener("submit", submitForm);
+  document.querySelectorAll(".text").forEach((elem) => {
+    elem.addEventListener("keyup", (e) => validatorHandler(e.target));
+  });
+}
+
+main();
