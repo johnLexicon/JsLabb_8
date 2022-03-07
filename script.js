582d30e79244e3cf81a6d5647cb2c1a7e5ec5dc3
John Lundgren <45228836+johnLexicon@users.noreply.github.com>
2022-03-07 16:05:30 +0100

    Added validation to form.

--- script.js
+++ script.js
@@ -1,81 +1,137 @@
-const validation = {
-  minLength: {
-    length: 2,
-    errorMessage: `To few characters`,
-    validate: function (text) {
-      return text.length >= this.length;
+const state = {
+  formData: {
+    fname: {
+      touched: false,
+      value: '',
+      validate() {
+        const text = this.value.trim();
+        if (text === '') return 'Full name is mandatory';
+        if (text.length < 2)
+          return 'Full name must be at least 2 characters long';
+        else return null;
+      }
     },
-  },
-  notEmpty: {
-    errorMessage: `A value is required`,
-    validate: (text) => text && text !== "",
-  },
-  minValue: {
-    value: 1,
-    errorMessage: `Value is to low`,
-    validate: function (value) {
-      return value >= this.value;
+    email: {
+      touched: false,
+      value: '',
+      validate() {
+        const text = this.value.trim();
+        const emailRegEx =
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/; //Email regex which accepts unicode.
+
+        if (text === '') return 'Full name is mandatory';
+        if (!emailRegEx.test(text)) return 'Invalid email address';
+        return null;
+      }
     },
+    gender: {
+      touched: false,
+      value: 0,
+      validate() {
+        const selectedIndex = this.value;
+        if (selectedIndex === 0) return 'Must choose gender';
+        return null;
+      }
+    },
+    tac: {
+      touched: false,
+      value: false,
+      validate() {
+        const checked = this.value;
+        if (!checked) return 'You must accept the terms and conditions';
+        return null;
+      }
+    }
   },
+  errors: []
 };
 
-function renderErrorMessages(target, errorMessages) {
-  for (message of errorMessages) {
-    target.insertAdjacentHTML("afterend", `<div class="errorMessage">${message}</div>`);
-  }
-}
+/** @type {HTMLFormElement} */
+const formElem = document.getElementById('registerForm');
+formElem.addEventListener('submit', handleSubmit);
+formElem.addEventListener('input', handleInput); // Triggered on every input, Works with all input elements and select element.
+formElem.addEventListener('focusout', handleFocusOut); // FocusOut event propagates.
 
-function validatorHandler(target) {
-  // If no validation rules for this target element.
-  if (!target.dataset.validators) {
-    return;
-  }
+/**
+ *
+ * @param {SubmitEvent} evt
+ */
+function handleSubmit(evt) {
+  evt.preventDefault();
 
-  // Clear earlier messages
-  target.parentElement.querySelectorAll(".errorMessage").forEach((e) => e.remove());
-  target.classList.remove("error");
-
-  const errorMessages = validate(target);
-  if (errorMessages.length > 0) {
-    target.classList.add("error");
-    renderErrorMessages(target, errorMessages);
-  }
-  return errorMessages;
-}
-
-function validate(target) {
-  const validators = target.dataset.validators.split(" ");
-  const errorMessages = [];
-  for (let validator of validators) {
-    let isValid = true;
-    isValid = validation[validator].validate(target.value);
-    if (!isValid) {
-      errorMessages.push(validation[validator].errorMessage);
-    }
-  }
-  return errorMessages;
-}
-
-function submitForm(e) {
-  e.preventDefault();
-  let errorMessages = [];
-  for (let i = 0; i < e.currentTarget.length; i++) {
-    const messages = validatorHandler(e.target[i]);
-    if (messages) errorMessages = errorMessages.concat(messages);
-  }
-  if (errorMessages.length > 0) {
-    console.log("Do not send");
-  } else {
-    console.log("Sent!");
-  }
-}
-
-function main() {
-  document.querySelector("form").addEventListener("submit", submitForm);
-  document.querySelectorAll("input").forEach((elem) => {
-    elem.addEventListener("keyup", (e) => validatorHandler(e.target));
+  const { formData } = state;
+  formElem.querySelectorAll('input,select').forEach((elem) => {
+    formData[elem.name].touched = true;
   });
-  document.querySelector("input[type=number]").addEventListener("change", (e) => validatorHandler(e.target));
+
+  doUpdate();
+
+  const notValid = state.errors.some((error) => error.message !== null);
+
+  if (notValid) {
+    console.log('Fix errors before submitting the form');
+  } else {
+    console.log('Submitted');
+  }
 }
 
-main();
+/**
+ *
+ * @param {InputEvent} evt
+ */
+function handleInput(evt) {
+  doUpdate();
+}
+
+/**
+ *
+ * @param {FocusEvent} evt
+ */
+function handleFocusOut(evt) {
+  const elem = evt.target;
+  const { formData } = state;
+
+  if (elem.name in formData) {
+    formData[elem.name].touched = true;
+    doUpdate();
+  }
+}
+
+function doUpdate() {
+  const { fname, email, gender, tac } = state.formData;
+  fname.value = formElem['fname'].value;
+  email.value = formElem['email'].value;
+  gender.value = formElem['gender'].selectedIndex;
+  tac.value = formElem['tac'].checked;
+
+  validateFormData();
+}
+
+function validateFormData() {
+  let { formData } = state;
+  const errors = [];
+  Object.keys(formData).forEach((prop) => {
+    if (!formData[prop].touched) return;
+    const errMessage = formData[prop].validate();
+    errors.push({ elemName: prop, message: errMessage });
+  });
+
+  state.errors = [...errors];
+  renderErrors();
+}
+
+function renderErrors() {
+  const { errors } = state;
+
+  errors.forEach((errorInfo) => {
+    const errorElem = formElem.querySelector(`#${errorInfo.elemName}-error`);
+    if (errorInfo.message) {
+      errorElem.innerText = errorInfo.message;
+      errorElem.classList.remove('d-none');
+      errorElem.classList.add('invalid');
+    } else {
+      errorElem.classList.add('d-none');
+      errorElem.classList.remove('invalid');
+    }
+  });
+}
